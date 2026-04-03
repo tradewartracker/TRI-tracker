bf33a7253b25bc05e7b6d303123c202ee8c4f001
mike <michael.e.waugh@gmail.com>
2026-04-03 12:40:02 -0400

    steel tariff update

--- main-tri-tracker.py
+++ main-tri-tracker.py
@@ -17,7 +17,7 @@ import base64
 # Alot of this was built off this "cross-fire demo"
 # https://github.com/bokeh/bokeh/blob/branch-2.3/examples/app/crossfilter/main.py
 
-final_month = 6
+final_month = 8
 final_year = 2026
 
 first_year = 2024
